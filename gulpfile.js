d79478ce20d5a8d97076dfaf01d75b75eec76166
João Pedro Spolier <joaopedrospolier@MacBook-Air-de-Joao.local>
2025-12-12 16:29:12 -0300

    setup do projeto

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const cleanCSS = require('gulp-clean-css');
 
-// Função que compila e minifica
 function styles() {
-  return gulp.src('./src/styles/**/*.scss')  // sua pasta de SCSS
-    .pipe(sass().on('error', sass.logError)) // compila Sass
-    .pipe(cleanCSS({ level: 2 }))            // minifica CSS
-    .pipe(gulp.dest('./dist/css'));          // saída
+    return gulp.src('./src/styles/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(gulp.dest('./dist/css'));
 }
 
-// Exporta a task explicitamente como "styles"
-exports.styles = styles;   // agora "gulp styles" funciona
-exports.default = styles;  // "gulp" funciona também
-exports.watch = function () {
-    gulp.watch('./src/styles/**/*.scss',gulp.parallel(styles));
+exports.default = styles;
+exports.watch = function() {
+  gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
