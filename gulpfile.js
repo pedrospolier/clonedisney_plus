fa4b1c1658ac1e2d89a14de5e27913f7a49e57d7
Joao Pedro Mariot Spolier <joaopedro_spolier@hotmail.com>
2025-12-29 14:53:54 -0300

    Criando o hero e importando a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,23 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
+// Task de estilos
 function styles() {
-    return gulp.src('./src/styles/*.scss')
-    .pipe(sass({ outputStyle: 'compressed' }))
+  return gulp.src('./src/styles/main.scss') // arquivo principal
+    .pipe(sass({ 
+      outputStyle: 'compressed',
+      includePaths: ['./src/styles']
+    }).on('error', sass.logError))
     .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
-exports.watch = function() {
-  gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+
+// Watch
+function watchFiles() {
+  gulp.watch('./src/styles/**/*.scss', styles); // observa tudo em styles
 }
+
+// Exporta tasks
+exports.styles = styles;
+exports.watch = watchFiles;
+exports.default = gulp.series(styles, watchFiles);
